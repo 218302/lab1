ad501356c9cf18fa1e556c429c4249b4ca45ee8a
218302 <218302@student.pwr.edu.pl>
2016-04-21 01:46:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,28 @@ void drawCharSquare(char **square, int n);
 int main(int argc, char **argv)
 {
 	int n;
-	drawCharSquare(charSquare(n), n);	
+	if(argc!=3)
+	{
+		printf("Incorrect arguments number. Exiting...");
+		exit(1);
+	}
+	n=atoi(argv[1]);
+	srand(atoi(argv[2]));
+	drawCharSquare(charSquare(n), n);
+	return 0;
 }
 
 char **charSquare(int n)
 {
 	char **square;
+	for(int i=0;i<n;i++) square[i]=(char*)malloc(n*(sizeof(char)));
+	for(long long i=0;i<n*n;i++) square[i/n][i%n]=rand()%26+97;
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for(long long i=0;i<n*n;i++) if(i%n==n-1 && i<n*n-1) printf("%c\n", square[i/n][i%n]); else printf("%c ", square[i/n][i%n]);
+	for(int i=0;i<n;i++) free(square[i]);
+	free(square);
 }
